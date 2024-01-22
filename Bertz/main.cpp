a184f74528496f22427dbf50728a9fa9ade10560
Simon Nilsson <simon@Simons-MacBook-Pro.local>
2024-01-22 11:10:21 +0100

    New folder

---
+++ Bertz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bertz
+//
+//  Created by Simon Nilsson on 2024-01-22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
